d0e49b355163f29d7837b01dd97d100db1361eb1
chugulina <gulina@MacBook-Air.local>
2024-07-14 20:38:48 +0600

    add cards

---
+++ src/components/Card.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Card({ book }) {
+  console.log(book);
+  return (
+    <>
+      {book.map((item) => {
+        let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail
+        let amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
+
+        if(thumbnail != undefined && amount != undefined) {
+            
+            return (
+                <div>
+              <div className="card">
+                <img src={thumbnail} alt="" />
+                <div className="bottom">
+                  <h3 className="title">{item.volumeInfo.title}</h3>
+                  <p className="amount">&#8377;{amount}</p>
+                </div>
+              </div>
+              </div>
+            );
+        }
+      })}
+    </>
+  );
+}
+
+export default Card;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
+import Card from './Card'
+import axios from 'axios'
 
 function Main() {
+    const [search, setSearch] = useState("")
+    const [bookData, setData] = useState([])
+    const searchBook=(evt) => {
+         if(evt.key === "Enter") {
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyC28erZBbkxcqzG7_Qr8uYIBOzVs5NnJuo'+'&maxResults=40')
+            .then(res => setData(res.data.items))
+            .catch(err => console.log(err))
+         }
+    }
   return (
+    // AIzaSyD5Z0YKFiCT-Wte3ZVT03hEKzSVqz6kfYU
+    
+    <div>
     <div className='header'>
         <div className="row1">
             <h1>A room without is like <br/> a body without a soul</h1>
@@ -10,13 +24,23 @@ function Main() {
         <div className="row2">
             <h2>Find Your Book</h2>
             <div className="search">
-                <input type="text" placeholder='Enter Your book name' />
-                <button><i class="fa-solid fa-magnifying-glass"></i></button>
+                <input type="text" placeholder='Enter Your book name' value={search} onChange={e => setSearch(e.target.value)}
+                onKeyPress={searchBook} />
+                <button><i className="fa-solid fa-magnifying-glass"></i></button>
             </div>
             <img src="./images/bg2.png" alt="" />
         </div>
     </div>
+        <div className="container">
+            <Card book={bookData} />
+ 
+
+        </div>
+    </div>
+
+    
   )
+ 
 }
 
 export default Main
